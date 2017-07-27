3a1b86e68b04b5a76b9cfbe2de860192d2371834
stla <laurent_step@yahoo.fr>
2017-07-27 18:37:53 +0200

    fix help KM31

--- R/datasets.R
+++ R/datasets.R
@@ -4,7 +4,7 @@
 #' @docType data
 #' @references Krishnamoorthy and Mathew, Statistical Tolerance Regions, Wiley 2009.
 #' @keywords data
-#' @usage data(KR31)
+#' @usage data(KM31)
 #' @format A data frame with 16 rows and 3 numerical columns.
 #' @examples 
 #' data(KM31)
